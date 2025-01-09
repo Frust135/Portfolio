82d0aa4b45fdde9943bff0a05e47c2f53f605c7b
Santiago Saavedra <mssr.99@hotmail.com>
2025-01-09 10:02:16 -0300

    Update paper

--- src/data/articles.js
+++ src/data/articles.js
@@ -1,10 +1,10 @@
 function article_1() {
 	return {
-		date: "[Under Review] LA-CCI 2024",
+		date: "LA-CCI 2024",
 		title: "Safety first: Toward safe action selection with contextual affordances	",
 		description:
 			"Reinforcement Learning empowers agents to make decisions in environments with the aim of maximizing a defined reward. In the area of robotic exploration, the challenge lies in enabling the agent to navigate its surroundings while avoiding dangerous states. Contextual affordance, a predictive model, anticipates the consequences of actions based on the agent’s current state. This model proves invaluable in guiding the agent away from dangerous situations. In the pursuit of enhancing robotic exploration safety, this paper examines the efficacy of a robotic agent employing contextual affordance to steer clear of unsafe states during exploration...",
-		link: "IEEE_LA-CCI_2024_paper_57",
+		link: "Safe_Action_Exploration_for_Robotic_Agents_Using_Contextual_Affordance",
 	};
 }
 

a4dd173501d249bd8ea068dc51b5fe459afa68b1
Santiago Saavedra <mssr.99@hotmail.com>
2025-01-09 14:25:23 -0300

    Update experience.js

--- src/data/experience.js
+++ src/data/experience.js
@@ -6,7 +6,7 @@ const EXPERIENCE = {
 			logo: "./tsoft.jpeg",
 			logoalt: "twitter",
 			subtitle: "Python Developer",
-            description: "- Developed robust APIs and automation solutions using Python. - Created a reusable chatbot framework with Langchain and GPT's LLM, streamlining processes for HR and Automation teams, deployed in Microsoft Teams using Azure. - Utilized Ollama AI model to automate document summarization, generating comprehensive knowledge bases in Confluence. - Automated processes using Kafka Consumers and Google Cloud Pub/Sub, enhancing operational efficiency and scalability.",
+            description: "- Automation and Scalable APIs: Designed and implemented robust APIs and automation solutions using Python, enhancing operational efficiency and scalability. - Reusable Chatbot Framework: Developed a highly configurable chatbot framework leveraging Langchain and GPT's LLM, enabling dynamic interaction for various business processes. These chatbots, deployed in Microsoft Teams using Azure Cloud, support multiple configurations (tool-oriented, data-oriented, and hybrid). - Knowledge Generation with RAG: Implemented a Retrieval-Augmented Generation (RAG) system using Milvus, enabling chatbots to efficiently access and utilize Confluence data, enhancing response accuracy and relevance. - Smart Document Management: Automated document summarization workflows using Ollama AI, generating structured knowledge bases in Confluence and improving information accessibility. - Operational Optimization: Automated key processes using Kafka Consumers and Google Cloud Pub/Sub, driving improvements in scalability and operational efficiency.",
 		},
         {
 			title: "Poptimize",
@@ -14,7 +14,7 @@ const EXPERIENCE = {
 			logo: "./poptimize.jpeg",
 			logoalt: "twitter",
 			subtitle: "Full Stack Developer",
-            description: "- Boosted school engagement by 20% by implementing new functionalities and APIs in a learning platform. - Developed RESTful APIs using Django to streamline data exchange and utilized jQuery for an engaging user interface. - Optimized services with AWS S3 for secure and scalable data management.",
+            description: "- Increased Engagement: Boosted school engagement by 20% by developing new functionalities and APIs for an educational platform. - API Design & Integration: Designed and implemented scalable RESTful APIs with Django, ensuring efficient data handling and seamless communication across the system architecture. - Dynamic User Interface: Delivered an engaging and responsive UI using jQuery, enhancing the platform’s user experience. - Cloud-Based Solutions: Optimized storage and cost management in AWS, leveraging S3 Buckets for secure and scalable data storage. Managed URL redirection and domain setup for poptimize.cl using AWS services to ensure smooth and reliable web traffic management. - Established CI/CD pipelines to improve continuous integration and software delivery, ensuring fast and secure deployments.",
 		},
 		{
 			title: "Universidad Central (CL)",
@@ -22,7 +22,7 @@ const EXPERIENCE = {
 			logo: "./ucen.png",
 			logoalt: "twitter",
 			subtitle: "Classroom Assistant",
-            description: "- Supported students in the Artificial Intelligence subject, addressing inquiries on linear regressions, neural networks, and convolutional neural networks for image classification.",
+            description: "- Provided support to Artificial Intelligence students, addressing inquiries on linear regression, neural networks, and convolutional neural networks for image classification.",
 		},
 	],
 };
